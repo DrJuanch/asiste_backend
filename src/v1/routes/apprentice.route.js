4dc8e58a2dca4eb27abc215f62b1d9035e84cd99
DrJuanch <juanjo88023@gmail.com>
2023-12-09 07:51:22 -0500

    Finishing

--- src/v1/routes/apprentice.route.js
+++ src/v1/routes/apprentice.route.js
@@ -6,7 +6,7 @@ router.post('/', controller.postApprentice);
 router.get('/', controller.getApprentice);
 router.put('/:id', controller.putApprentice);
 router.delete('/:id', controller.deleteApprentice);
-router.put('/:id', controller.addAttendance);
+router.put('/añadirAsistencia/:id', controller.addAttendance);
 
 
 module.exports = router;
